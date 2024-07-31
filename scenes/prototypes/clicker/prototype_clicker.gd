e28245b214def620f23cdef8daf7195d2ec20bc4
Allan Chiang <allanchiang24@gmail.com>
2024-07-31 17:55:33 +1200

    Move on_button_pressed function to bottom for organization reasons

--- scenes/prototypes/clicker/prototype_clicker.gd
+++ scenes/prototypes/clicker/prototype_clicker.gd
@@ -7,10 +7,6 @@ extends Control
 var stardust : int = 0
 
 
-func _on_button_pressed() -> void:
-	create_stardust()
-
-
 func create_stardust() -> void:
 	stardust += 1
 	update_label_text()
@@ -18,3 +14,7 @@ func create_stardust() -> void:
 
 func update_label_text() -> void:
 	label.text = "Stardust : %s" %stardust
+
+
+func _on_button_pressed() -> void:
+	create_stardust()
